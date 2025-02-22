d2ce1c5f8975e2f9b50dbcc10ec20330844df29a
irvyn2703 <tosolatoso17@gmail.com>
2025-02-21 20:51:10 -0600

    style(moviesApp): update card styles

--- MoviesApp/src/components/Card/UI/CardUI.jsx
+++ MoviesApp/src/components/Card/UI/CardUI.jsx
@@ -1,7 +1,7 @@
 export default function CardUI({ title, year, poster, type }) {
   return (
-    <div className="rounded-xl bg-blue-900 w-fit h-auto overflow-hidden">
-      <img src={poster} alt={title} />
+    <div className="rounded-xl bg-blue-900 h-auto overflow-hidden w-80 flex flex-col justify-between min-h-60">
+      <img src={poster} alt={`portada de la pelicula ${title}`} />
       <div className="flex flex-col items-center p-2 gap-2">
         <h2 className="font-bold text-blue-50">{title}</h2>
         <p className="text-blue-50">
