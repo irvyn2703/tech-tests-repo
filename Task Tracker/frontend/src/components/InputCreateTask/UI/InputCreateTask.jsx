1405b8094dae39b9d26201d52dd7ae8917435344
irvyn2703 <tosolatoso17@gmail.com>
2025-02-26 20:05:32 -0600

    feat(taskTracker): create input to create task

--- Task Tracker/frontend/src/components/InputCreateTask/UI/InputCreateTask.jsx
+++ Task Tracker/frontend/src/components/InputCreateTask/UI/InputCreateTask.jsx
@@ -1,10 +1,19 @@
 export default function InputCreateTask() {
   return (
-    <header>
-      <label htmlFor="newTask">Task Tracker</label>
-      <div>
-        <input type="text" name="newTask" />
-        <button>✒️</button>
+    <header className="grid gap-7">
+      <label htmlFor="newTask" className="text-2xl">
+        Task Tracker
+      </label>
+      <div className="flex border-2 rounded-lg">
+        <input
+          type="text"
+          name="newTask"
+          className="flex-grow p-2 focus:outline-none text-2xl overflow-ellipsis"
+          placeholder="Start writing and press enter to create task"
+        />
+        <button className="bg-black p-2 m-2 flex-shrink-0 rounded-lg text-blue-50">
+          Enter
+        </button>
       </div>
     </header>
   );
--- Task Tracker/frontend/src/components/TaskContainer/UI/TaskContainer.jsx
+++ Task Tracker/frontend/src/components/TaskContainer/UI/TaskContainer.jsx
@@ -3,13 +3,13 @@ import Task from "../../Task/UI/Task";
 
 export default function TaskContainer() {
   return (
-    <>
+    <div className="w-11/12 m-auto mt-10">
       <InputCreateTask />
       <main>
         <Task />
         <Task />
         <Task />
       </main>
-    </>
+    </div>
   );
 }
