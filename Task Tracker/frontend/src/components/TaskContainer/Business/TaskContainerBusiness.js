00294c517f50d83118bbe0efa2e13fa8cfcd3ce6
irvyn2703 <tosolatoso17@gmail.com>
2025-02-27 19:49:34 -0600

    refactor(taskTracker): removed console logs

--- Task Tracker/frontend/src/components/TaskContainer/Business/TaskContainerBusiness.js
+++ Task Tracker/frontend/src/components/TaskContainer/Business/TaskContainerBusiness.js
@@ -4,7 +4,6 @@ export default function TaskContainerBusiness() {
   const [newTask, setNewTask] = useState(true);
 
   const handleToggleNewTask = () => {
-    console.log("handleToggleNewTask");
     setNewTask(!newTask);
   };
 
