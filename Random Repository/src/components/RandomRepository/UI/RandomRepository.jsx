5a4ee1359e8ac0d4ef677e9ea9212aa7134d2162
irvyn2703 <tosolatoso17@gmail.com>
2025-03-07 16:05:42 -0600

    feat(RandomRepository): display repository details

--- Random Repository/src/components/RandomRepository/UI/RandomRepository.jsx
+++ Random Repository/src/components/RandomRepository/UI/RandomRepository.jsx
@@ -6,17 +6,34 @@ export default function RandomRepository({ select }) {
 
   console.log(repository);
   if (select && !isLoading && repository)
-    return !repository.errors ? (
-      <div className="bg-gray-300 min-h-40 rounded-2xl flex justify-center items-center text-2xl">
-        Data
-      </div>
-    ) : (
+    return (
       <>
-        <div className="bg-red-300 min-h-40 rounded-2xl flex justify-center items-center text-2xl">
-          Error fetching repositories
+        <div
+          className={`${
+            !repository.error ? "bg-gray-300" : "bg-red-300"
+          } min-h-40 rounded-2xl flex justify-center items-center text-2xl`}
+        >
+          {!repository.errors ? (
+            <div>
+              <h2>{repository.repositoryName}</h2>
+              <p>{repository.description}</p>
+              <div>
+                <span>{repository.language}</span>
+                <span>{repository.stars}</span>
+                <span>{repository.forks}</span>
+                <span>{repository.issues}</span>
+              </div>
+            </div>
+          ) : (
+            "Error fetching repositories"
+          )}
         </div>
-        <button className="bg-red-500 rounded-2xl text-white py-3 text-2xl cursor-pointer">
-          Click to retry
+        <button
+          className={`${
+            !repository.error ? "bg-gray-900" : "bg-red-300"
+          } rounded-2xl text-white py-3 text-2xl cursor-pointer`}
+        >
+          {!repository.errors ? "Refresh" : "Click to retry"}
         </button>
       </>
     );
