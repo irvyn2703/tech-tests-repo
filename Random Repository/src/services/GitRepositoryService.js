58b9b894bc33c9771632bac7eeaaaea4d31d8498
irvyn2703 <tosolatoso17@gmail.com>
2025-03-07 19:14:12 -0600

    fix(GitRepositoryService): retry fetching repository with a random page when response containts no item

--- Random Repository/src/services/GitRepositoryService.js
+++ Random Repository/src/services/GitRepositoryService.js
@@ -2,6 +2,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 async function getRepository({ language, numPage }) {
   let URL = `${API_URL}?q=language:${language}&per_page=1&page=${numPage}`;
+
   try {
     const response = await fetch(URL);
 
@@ -11,7 +12,18 @@ async function getRepository({ language, numPage }) {
       );
     }
 
-    return await response.json();
+    const data = await response.json();
+
+    if (data.items.length === 0 && data.total_count > 0) {
+      const maxPage = data.total_count;
+      const newNumPage = Math.floor(Math.random() * maxPage) + 1;
+
+      if (newNumPage !== numPage) {
+        return getRepository({ language, numPage: newNumPage });
+      }
+    }
+
+    return data;
   } catch (error) {
     console.error(error);
   }
